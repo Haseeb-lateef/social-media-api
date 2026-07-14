61669d08fedc17e682a715b20cdd44fc4676c712
haseeb lateef <lateefhaseeb1@gmail.com>
2026-07-15 00:10:38 +0100

    added requirems and cors

--- app/main.py
+++ app/main.py
@@ -3,11 +3,24 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routers import post, user, auth, vote
+from fastapi.middleware.cors import CORSMiddleware
 
+
+origins = ["*"]
 
 app = FastAPI() 
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
+@app.get("/")
+def root():
+    return {"message": "hello"}
 
 
 
